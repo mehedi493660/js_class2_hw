32ed431e8edb31f9563ea370bc38102fa3a631ef
mehedi493660 <144321420+mehedi493660@users.noreply.github.com>
2023-10-17 21:34:52 +0600

    Add files via upload

---
+++ js class2/class2.js
@@ -0,0 +1,47 @@
+var total = 7532;
+var extra = 560;
+var rina  = (total-extra)/2;
+var mina = rina+extra;
+
+console.log('rina have '+rina);
+console.log('mina have '+mina);
+
+
+
+var egg  = 8;
+var cost = 72;
+var to_buy = 15;
+
+var each_egg = cost/egg;
+var all_cost = each_egg*to_buy;
+
+console.log('result = '+ all_cost)
+
+
+
+var money = 3000;
+var prawn = 480;
+var shingh = 550;
+var prawn_needs = 3;
+var shingh_needs = 2;
+
+var cost_of_prown = prawn*prawn_needs;
+var cost_of_shingh = shingh*shingh_needs;
+
+var total = cost_of_prown+cost_of_shingh;
+var back = money-total;
+
+console.log('return = '+ back)
+
+
+var total_age = 80;
+var suppose_daugther = 1;
+var suppose_father = 4;
+var age_is = suppose_daugther+suppose_father;
+
+var d_age = total_age/age_is;
+var f_age = d_age*4;
+
+console.log('daughters age = '+ d_age);
+console.log('fatheres age = '+ f_age);
+
